645081ce07c568b5b386b7ded4c2f55892d3c16d
Om-Patel-BD <omdevangi3@gmail.com>
2025-09-04 10:27:17 +0530

    Add Vercel serverless entrypoint

--- api/index.py
+++ api/index.py
@@ -3,4 +3,4 @@ import os
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myportfolio.settings")
-app = get_asgi_application()  # Vercel looks for 'app'
+app = get_asgi_application()  # Vercel looks for "app"
