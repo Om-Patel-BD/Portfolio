3f624733041bef700dc37ffb7c0fe21b57c53b69
Om-Patel-BD <omdevangi3@gmail.com>
2025-09-04 10:35:55 +0530

    Final Vercel setup for Django

--- api/index.py
+++ api/index.py
@@ -1,6 +1,5 @@
-# api/index.py
 import os
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myportfolio.settings")
-app = get_asgi_application()  # Vercel looks for "app"
+app = get_asgi_application()
